db5b401187569e7a1088ce7f73f2409b564acbca
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-18 15:39:42 +0100

    exercise-js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let userName = prompt ("Qual è il tuo nome?"); //NAME
+console.log (userName) //CONSOLE PRINT
+
+let surName = prompt ("Qual è il tuo cognome") //SURNAME
+console.log (surName); //CONSOLE PRINT
+
+let colorFavourite = prompt ("Qual è il tuo colore preferito?") //FAVOURITE COLOR
+console.log (colorFavourite); //CONSOLE PRINT
+
+const number = 22 //CONSTANT NUMBER
+//RESULT
+document.getElementById("name") .innerHTML = userName + " " + surName + " " + colorFavourite + " " + number
